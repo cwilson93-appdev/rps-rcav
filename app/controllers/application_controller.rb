96e105f1673ccddb7b18c46afdd555015ced3412
cwilson93 <cwilson2@chicagobooth.edu>
2022-10-21 20:05:46 +0000

    end of video

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,30 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({:template=> "game_templates/rules.html.erb"})
+  end
 
   def play_rock
     render({:template=> "game_templates/user_rock.html.erb"})
+  end
+
+  def play_paper
+    render ({:template=> "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    render ({:template=> "game_templates/user_scissors.html.erb"})
   end
 end
